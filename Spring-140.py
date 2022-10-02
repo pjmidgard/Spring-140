8880826ed07d8df64ad1a1c10d30ce9211ce37fb
Java Software <pj.midgard@yahoo.com>
2022-10-02 16:02:19 +0100

    Update Spring-140.py

--- Spring-140.py
+++ Spring-140.py
@@ -194,37 +194,29 @@ class encypthion_class:
                                 
                                 #print(info_hex)
                                 
-                                #011...
-                                #01
-                                #00xx
-                                #11xx
-                                #1110
-                                #11110...
+                             
                                 
-                                #011111111
-                                #01111111
-                                #0111111
-                                #011111
-                                #01111
-                                #0111
-                                #011
-                                #01
+                                #111111110
+                                #11111110
+                                #1111110
+                                #111110
+                                #11110
+                                #1110
+                                #110
+                                #10
                                 #0000
-                                #0001
                                 #0010
-                                #0011
                                 #0100
-                                #0101
                                 #0110
-                                #0111
-                                #1000
                                 #1001
                                 #1010
                                 #1011
                                 #1100
                                 #1101
                                 #1110
-                                #11110
+                                #01111
+                                
+                                
                                 
  
                                 lower="abcdef01"
@@ -240,8 +232,8 @@ class encypthion_class:
                                         if(string[i] in lower):
                                  
                                  
-                                            res+="0"
-                                            res+="1"*(lower.index(string[i])+1)
+                                            res+="1"
+                                            res+="0"*(lower.index(string[i])+1)
                                             
                                            
                                            
@@ -257,18 +249,19 @@ class encypthion_class:
                                  
                                             #res+="0"*(upper.index(string[i])+1)
                                             res1+="0"*(upper.index(string[i])+1)
-                                            
                                             long_res=len(res1)
+                                            
                                             long_res=long_res-1
                                             b=format(long_res,'03b')
                                             if long_res==7:
-                                                res+="11110"
+                                                res+="01111"
                                             elif long_res<4:
+                                                res+=b 
                                                 res+="0"
-                                                res+=b
                                             else:
-                                                res+="1"
+                                                
                                                 res+=b
+                                                res+="1"
                                             res1=""
                                             
                                  
@@ -276,9 +269,9 @@ class encypthion_class:
                                  
                                         if(string[i] in lower):
                                             
-                                            res+="0"
+                                            res+="1"
                                  
-                                            res+="1"*(lower.index(string[i])+1)
+                                            res+="0"*(lower.index(string[i])+1)
                                             
                                            
                                             
@@ -295,13 +288,14 @@ class encypthion_class:
                                             long_res=long_res-1
                                             b=format(long_res,'03b')
                                             if long_res==7:
-                                                res+="11110"
+                                                res+="01111"
                                             elif long_res<4:
+                                                res+=b 
                                                 res+="0"
-                                                res+=b
                                             else:
-                                                res+="1"
+                                                
                                                 res+=b
+                                                res+="1"
                                             res1=""
                                  
                                 info_hex=res
@@ -368,220 +362,3 @@ class encypthion_class:
                                         x2 = time()
                                         x3=x2-x
                                         return print(x3)
-
-    def encypthion2(self):
-
-                 
-                 if namez=="e":
-                    
-                    name = input("What is name of file? ")
-                    if os.path.exists(name):
-                            print('Path is exists!')
-                    else:
-                            print('Path is not exists!')
-                            raise SystemExit
-                    Portal=2
-                    namea=""
-                    namem=""
-                    namema="?"
-                    Deep=0
-                 
-                    assxw=0
-                    blockw=5
-                    blockw1=4
-                    nameas=name
-                    nac=len(nameas)
-                    name_cut=""
-                    name_cut=len(".bin")
-                    nameas=name
-                    
-                    name_long=len(nameas)
-                    nameSS=name[name_long-name_cut:]
-                    if nameSS!=".bin":
-                        x3=0.0
-                        return print(x3)
-                        
-                    nameas=name[:name_long-name_cut]
-                    nac=len(nameas)
-                    
-                  
-                    
-                    long=len(nameas)
-
-                    nac=len(nameas)
-                   
-                    countraz=0
-                    cvf=2
-                    cvf1=0
-                    s=""
-                    
-                 
-                    size_data3=""
-                    size_data2=""
-
-                    sscvf=0
-                    
-                    qqqqwzl=0
-
-                    block=1
-
-                    x=0
-                    x1=0
-                    x2=0
-                    x = time()
-
-                    with open(nameas, "w") as f4:
-                            f4.write(s)
-                    with open(nameas, "a") as f3:
-                            f3.write(s)
-                    with open(name, "rb") as binary_file:
-
-                       # Read the whole file at once
-                        
-                        data = binary_file.read()
-                        import paq
-                        data= paq.decompress(data)
-
-                        
-
-                    
-                        if len(data)==0:
-                            x4=0.0
-                            print(x4)
-                            raise SystemExit
-                     	
-
-                        s=str(data)
-                       
-                        lenf1=len(data)
-                        lenf5=len(data)
-                        
-                        assx=0
-                        qqqwz=0
-                       
-                        while assx<10:
-                       
-                            aas1=0
-                            a1=0
-
-                            cvf=cvf+1
-                            
-                            countraz=countraz+1
-
-                            with open(nameas, "ab") as f2:
-                                if countraz==1:
-                                    size_data=bin(int(binascii.hexlify(data),16))[2:]
-                                    lenf=len(size_data)
-                                    lenf1=len(data)
-                                
-                                    xc=(lenf1*8)-lenf
-                                    z=0
-                                    if xc!=0:
-                                        while z<xc:
-                                            size_data="0"+size_data
-                                            z=z+1
-                                            
-                                   
-
-                                    if countraz==1:
-                                        size_data2=size_data
-                            
-                                    n = int(size_data2, 2)
-                                
-                                    qqwslenf=len(size_data2)
-                                    qqwslenf=(qqwslenf/8)*2
-                                    qqwslenf=str(qqwslenf)
-                                    qqwslenf="%0"+qqwslenf+"x"
-                             
-                                    jl=binascii.unhexlify(qqwslenf % n)
-                                    sssssw=len(jl)
-                                    
-                                    data=jl
-                                    qqqwz=qqqwz+1
-                                   
-                                    if countraz==1:
-
-                                        lenf5=len(data)
-
-                                    size_data=bin(int(binascii.hexlify(data),16))[2:]
-                                    lenf=len(size_data)
-
-                                    lenf1=len(data)
-                                
-                                    xc=(lenf1*8)-lenf
-                                    z=0
-                                    if xc!=0:
-                                        while z<xc:
-                                            size_data="0"+size_data
-                                            z=z+1
-
-                                    size_data2=size_data
-
-                                    lenf3=len(size_data2)
-                                lenf2=len(size_data2)  
-                                x4=1
-                                if x4==1:
-
-                                    
-
-                                    size_data3=size_data2
-                                    
-                                    if size_data3[0:9]=="000000001":
-                                        size_data3=size_data3[9:]
-                                    elif size_data3[0:8]=="00000001":
-                                        size_data3=size_data3[8:]
-                                    elif size_data3[0:7]=="0000001":
-                                        size_data3=size_data3[7:]
-                                    elif size_data3[0:6]=="000001":
-                                        size_data3=size_data3[6:]
-                                    elif size_data3[0:5]=="00001":
-                                        size_data3=size_data3[5:]
-                                    elif size_data3[0:4]=="0001":
-                                        size_data3=size_data3[4:]
-                                    elif size_data3[0:3]=="001":
-                                        size_data3=size_data3[3:]
-                                    elif size_data3[0:2]=="01":
-                                        size_data3=size_data3[2:]
-                                    elif size_data3[0:1]=="1":
-                                        size_data3=size_data3[1:]                                    
-                                    
-                                   
-                                        
-                                      
-                                    n = int(size_data3, 2)
-                                    
-                                    
-                                    qqwslenf=len(size_data3)
-                                    qqwslenf=(qqwslenf/8)*2
-                                    qqwslenf=str(qqwslenf)
-                                    qqwslenf="%0"+qqwslenf+"x"
-                             
-                                    jl=binascii.unhexlify(qqwslenf % n)
-                                  
-                                    sssssw=len(jl) 
-                                  
-                                   
-
-                                    qqqwz=qqqwz+1
-                                    szxzzza=""
-                                    szxzs=""
-                                    
-                            
-                                    assxw=assxw+1
-                                    if assxw==1:
-                                        assx=10
-                                        if assx==10:
-                                        	
-                                            f2.write(jl)
-                                            x2 = time()
-                                            x3=x2-x
-                                            return print(x3)  
-                  
-self=""                                
-d=encypthion_class()
-    
-xw=d.encypthion1()
-print(xw)
-
-xw1=d.encypthion2()
-print(xw1)
